de99d5f534aea20b20992b02bc9b6ce49a106c7e
harinugraha129 <harin260@gmail.com>
2020-07-16 18:47:52 +0700

    menambah fiture

---
+++ README.md
@@ -0,0 +1,23 @@
+Start a clustering k-Means pixel image
+
+### Demo
+
+```bash
+optional arguments:
+  -h, --help            show this help message and exit
+  -i IMAGE, --image IMAGE
+                        image path
+  -o OUTPUT, --output OUTPUT
+                        image output
+  -k K_VALUE, --k_value K_VALUE
+                        k value of k-Means
+
+# k = 2
+python main.py -i input/image.jpg -o result.jpg -k 2
+```
+### Example
+##### image input
+<p align="center"><img src="input/image.jpg" width="640"\></p>
+
+##### image output
+<p align="center"><img src="output/result.jpg" width="640"\></p>
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import argparse
 import numpy as np
 import cv2
 
-img = cv2.imread('image.jpg')
+parser = argparse.ArgumentParser(description="Start a clustering k-Means pixel image")
+parser.add_argument("-i", "--image", help="image path", type=str)
+parser.add_argument("-o","--output", help="image output", type=str, default="result.jpg")
+parser.add_argument("-k", "--k_value", default="2",  help="k value of k-Means", type=int)
+args = parser.parse_args()
+
+# input image
+img = cv2.imread(args.image)
 Z = img.reshape((-1,3))
 
 # convert to np.float32
@@ -9,7 +17,7 @@ Z = np.float32(Z)
 
 # define criteria, number of clusters(K) and apply kmeans()
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-K = 5
+K = args.k_value
 ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
 
 # Now convert back into uint8, and make original image
@@ -17,7 +25,9 @@ center = np.uint8(center)
 res = center[label.flatten()]
 res2 = res.reshape((img.shape))
 
-cv2.imwrite('result.jpg',res2)
+# save result image
+path = "output/"+args.output
+cv2.imwrite(path,res2)
 
 cv2.imshow('res2',res2)
 cv2.waitKey(0)
